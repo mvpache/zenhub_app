65eed58f0aabd00d9cfaa9b1a105435deb09773e
unknown <admin@ITLAPTOP.TBOX.local>
2019-08-03 09:50:15 -0700

    built index file for express

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+
+const server = express();
+
+// Serve static files from the React app
+server.use(express.static(path.join(__dirname, 'client/build')));
+
+// Put all API endpoints under '/api'
+server.get('/api/', (req, res) => {
+
+    // Generate some passwords
+    const response = 'hello';
+
+    // Return them as json
+    res.json(response);
+
+
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+server.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+server.listen(port);
+
+console.log(`Server listening on ${port}`);
